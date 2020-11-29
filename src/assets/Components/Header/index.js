542badabe440ad609ac583d3202ee749241556c8
Luma Montes <lumagoesmontes@gmail.com>
2020-11-29 01:00:10 -0300

    estruturaçao do componente header

--- src/assets/Components/Header/index.js
+++ src/assets/Components/Header/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../../images/logo.jpg'
+import { FaBars } from 'react-icons/fa';
+
+import './style.css'
 
 function Header (props) {
     return (
         <header id="header-container">
              <div className="logo-container">
-                {/* <img src={} alt=""/> */}         
+                <img src={Logo} alt="Hotel Brasil"/>         
             </div>
 
              <div className="header-content">
-                    <Link to="/"></Link>
-                    <Link to="/"></Link>
-                    <Link to="/"></Link>
+                    <Link to="/">Quem somos</Link>
+                    <Link to="/">Quartos</Link>
+                    <Link to="/">Galeria</Link>
+                    <Link to="/">Contatos</Link>
             </div>
+            <FaBars className="menu-bars"></FaBars>
+
         </header>
         )
 }
