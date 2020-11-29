3a6e3bdbd3ac2926608370a9b5bc38718bc1b6a8
Luma Montes <lumagoesmontes@gmail.com>
2020-11-29 01:02:43 -0300

    finalizado landing page

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import './style.css';
 import Header from '../../assets/Components/Header'
-function Landing () {
+function Landing() {
     return (
         <section id="landing-container" className="container">
-            <div id="homepage-container">
-                <Header/>
-
-                <div id="homepage-text-content">
+            <div className="homepage-container">
+                <Header />
+                <div className="homepage-text-content">
                     <h2> Bem vindo ao</h2> <br></br>
                     <h1 className="title">Hotel Brasil</h1>
                 </div>
             </div>
+
+            
         </section>
-
-
-        
     )
 }
 
