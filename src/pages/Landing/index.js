a115607a18bc1be7035002fad9bd22de9bdc0d33
Luma Montes <lumagoesmontes@gmail.com>
2020-11-29 16:27:36 -0300

    adicionado icones e mapa

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -5,6 +5,12 @@ import CamCard from '../../assets/Components/ComCard';
 import RoomCard from '../../assets/Components/RoomCard'
 import Footer from '../../assets/Components/Footer';
 import ImgRoom from '../../assets/images/room.png';
+import WifiIcon from '../../assets/images/wifi.svg';
+import CoffeIcon from '../../assets/images/coffee-cup.svg';
+import EnergyIcon from '../../assets/images/lighting.svg';
+import { MapContainer, TileLayer} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
 
 function Landing() {
     return (
@@ -20,9 +26,9 @@ function Landing() {
             <div className="amenities-content container">
                 <h2 className="title"> Comodidades</h2>
                 <div className="camcards">
-                    <CamCard comodidade="Wifi"></CamCard>
-                    <CamCard comodidade="Café Regional"></CamCard>
-                    <CamCard comodidade="Gerador"></CamCard>
+                    <CamCard image={WifiIcon} comodidade="Wifi"></CamCard>
+                    <CamCard image={CoffeIcon} comodidade="Café Regional"></CamCard>
+                    <CamCard image={EnergyIcon} comodidade="Gerador"></CamCard>
                 </div>
             </div>
 
@@ -35,7 +41,6 @@ function Landing() {
                 </div>
 
             </div>
-
             <div className="about-container container">
                 <h2 className="title">Sobre</h2>
 
@@ -53,10 +58,8 @@ function Landing() {
             </div>
 
             <div className="gallery container">
-                <h2 className="title">Galeria</h2>
+                <h2 className="title">Galeriaaaa</h2>
                 <figure>
-                    <img src={ImgRoom} alt="Imagem Quarto"/>
-                    <img src={ImgRoom} alt="Imagem Quarto"/>
                     <img src={ImgRoom} alt="Imagem Quarto"/>
                     <img src={ImgRoom} alt="Imagem Quarto"/>
                     <img src={ImgRoom} alt="Imagem Quarto"/>
@@ -64,7 +67,19 @@ function Landing() {
                     <img src={ImgRoom} alt="Imagem Quarto"/>
                 </figure>
             </div>
-
+            <div className="map-content">
+            <MapContainer 
+            center={[0.048897,-51.1004567]}
+            zoom={15}
+            style={{width: '100%', height: '100%'}}
+            >
+            
+            <TileLayer 
+            url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibHVtYW1vbnRlcyIsImEiOiJja2c2enN2bGgwMmZrMnFwYmsyMnd1eDJpIn0.j3nJhNinwAXTGG1Qfe6kPQ`}
+            />
+            </MapContainer>
+            
+            </div>
             <Footer></Footer>
         </section>
     )
