af408e10d3e460b47075eff648639233babbea52
Luma Montes <lumagoesmontes@gmail.com>
2020-11-29 16:24:58 -0300

    iniciado estruturaçao do footer

--- src/assets/Components/Footer/index.js
+++ src/assets/Components/Footer/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
+import { FaInstagramSquare, FaWhatsappSquare, FaFacebookSquare, FaEnvelope, FaArrowAltCircleUp } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
+import './style.css'
 
-function Footer () {
+function Footer() {
     return (
-        <div>Footer aqui</div>
+        <footer className="footer-content">
+            <div className="contacts">
+            <h1 className="title">Hotel Brasil</h1>
+                <div>
+                    <FaEnvelope className="icon"></FaEnvelope>
+                    <Link to=""> lorem ipsum </Link>
+                </div>
+
+                <div>
+                    <FaInstagramSquare></FaInstagramSquare>
+                    <Link to="">lorem ipsum</Link>
+                </div>
+
+                <div>
+                    <FaWhatsappSquare></FaWhatsappSquare>
+                    <Link to="">lorem ipsum</Link>
+                </div>
+
+                <div>
+                    <FaFacebookSquare></FaFacebookSquare>
+                    <Link to="">lorem ipsum</Link>
+                </div>
+            </div>
+
+                <Link to="">feito por nois</Link>
+
+                <div className="scroll-up">
+                    <Link to="">
+                    <FaArrowAltCircleUp></FaArrowAltCircleUp>
+                    </Link>
+                </div>
+        </footer>
     )
 }
 
