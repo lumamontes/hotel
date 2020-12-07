14a8b4776e5357f3b095a41722e5d628355b60a0
Luma Montes <lumagoesmontes@gmail.com>
2020-12-06 22:46:17 -0300

    criado componente para links da navbar

---
+++ src/assets/Components/Header/MenuItens.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Quem somos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Quartos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Galeria',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contatos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Pré-Reservar',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
